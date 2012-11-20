991af994ba7b2f396268b982d71e59f199f54737
Ti53 <soporte@ti53.es>
2012-11-20 14:03:53 +0100

    staticpagescontroller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '937de8446295db17d5b0156b486e6d1753df5eb2c1837ce8a177d67f7847eab92b3ca0812a4bb24e4bb4b8ad748c51df1d1b0737e0ca3792a91942f5fb044471'
