4738fca2cf02f5ace15e6d446fed6ca24f55d80d
Ti53 <soporte@ti53.es>
2012-11-21 11:51:25 +0100

    http://ruby.railstutorial.org/chapters/static-pages?version=3.2#sec-static_pages_exercises
    http://ruby.railstutorial.org/chapters/static-pages?version=3.2#sec-static_pages_exercises

--- spec/requests/static_p_ages_spec.rb
+++ spec/requests/static_p_ages_spec.rb
@@ -1,40 +1,46 @@
-#require 'spec_helper'
-
-#describe "StaticPAges" do
-#  describe "GET /static_p_ages" do
-#    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get static_p_ages_index_path
-#      response.status.should be(200)
-#    end
-#  end
-#end
-
 require 'spec_helper'
 
 describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_p_ages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_p_ages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
-  
+
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_p_ages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_p_ages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
-  
+
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_p_ages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_p_ages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
